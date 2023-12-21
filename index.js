c922ac40ba6dbd266d101102ea5288ab2c77f19e
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-12-21 13:35:22 +0200

    0^0=1 & final alert numInput.trim()

--- index.js
+++ index.js
@@ -6,21 +6,23 @@
 - Для від'ємних ступенів має рахувати (x^n = 1/x^(-n) ), для нецілих ступенів має писати помилку 
 */
 
-// debugger;
+debugger;
 
 function pow(num, degree) {
-  // if (degree === 0) {
-  //   return 1;
-  // } else if (degree > 0) {
-  //   return num * pow(num, degree - 1);
-  // } else {
-  //   return 1 / (num * pow(num, -degree - 1));
-  // }
-  return degree === 0
-    ? 1
-    : degree > 0
-    ? num * pow(num, degree - 1)
-    : 1 / (num * pow(num, -degree - 1));
+  if (degree === 0) {
+    return 1;
+  } else if (degree > 0) {
+    return num * pow(num, degree - 1);
+  } else {
+    return 1 / (num * pow(num, -degree - 1));
+  }
+  // return degree === 0
+  //   ? 1
+  //   : degree === 0 && num === 0
+  //   ? 1
+  //   : degree > 0
+  //   ? num * pow(num, degree - 1)
+  //   : 1 / (num * pow(num, -degree - 1));
 }
 
 let numInput = prompt('Введіть число, які хочете возвести в ступінь.');
@@ -34,48 +36,44 @@ while (
     !degreeInput?.trim() ||
     isNaN(numInput) ||
     isNaN(degreeInput) ||
-    !Number.isInteger(parseInt(degreeInput)))
+    !Number.isInteger(parseFloat(degreeInput)))
 ) {
-  numInput = !numInput?.trim()
-    ? prompt('Пусте значення не можна возвести в ступінь. Введіть число!')
-    : !isNaN(numInput)
-    ? numInput
-    : prompt('Лише число можна возвести в ступінь. Введіть число!');
+  // numInput = !numInput?.trim()
+  //   ? prompt('Пусте значення не можна возвести в ступінь. Введіть число!')
+  //   : !isNaN(numInput)
+  //   ? numInput
+  //   : prompt('Лише число можна возвести в ступінь. Введіть число!');
 
-  degreeInput = !degreeInput?.trim()
-    ? prompt('Значення ступені не може бути пустим. Введіть ціле число!')
-    : !isNaN(degreeInput) && Number.isInteger(parseFloat(degreeInput))
-    ? degreeInput
-    : prompt(
-        'Ступінь має бути цілим числом. Введіть ціле числове значення ступеня!'
-      );
+  // degreeInput = !degreeInput?.trim()
+  //   ? prompt('Значення ступені не може бути пустим. Введіть ціле число!')
+  //   : !isNaN(degreeInput) && Number.isInteger(parseFloat(degreeInput))
+  //   ? degreeInput
+  //   : prompt(
+  //       'Ступінь має бути цілим числом. Введіть ціле числове значення ступеня!'
+  //     );
 
-  // if (!numInput?.trim()) {
-  //   numInput = prompt(
-  //     'Пусте значення не можна возвести в ступінь. Введыть число!'
-  //   );
-  // } else if (!degreeInput?.trim()) {
-  //   degreeInput = prompt(
-  //     'Значення ступені не може бути пустим. Введіть ціле число!'
-  //   );
-  // } else if (isNaN(numInput)) {
-  //   numInput = prompt('Лише число можна возвести в ступінь. Введіть число!');
-  // } else {
-  //   degreeInput = prompt(
-  //     'Ступінь має бути цілим числом. Введіть ціле число значення ступені!'
-  //   );
-  // }
+  if (!numInput?.trim()) {
+    numInput = prompt(
+      'Пусте значення не можна возвести в ступінь. Введыть число!'
+    );
+  } else if (!degreeInput?.trim()) {
+    degreeInput = prompt(
+      'Значення ступені не може бути пустим. Введіть ціле число!'
+    );
+  } else if (isNaN(numInput)) {
+    numInput = prompt('Лише число можна возвести в ступінь. Введіть число!');
+  } else {
+    degreeInput = prompt(
+      'Ступінь має бути цілим числом. Введіть ціле число значення ступені!'
+    );
+  }
 }
 
 if (numInput === null || degreeInput === null) {
   alert('Відміна');
 } else {
-  const result = pow(numInput, degreeInput);
+  const result = pow(parseFloat(numInput), parseInt(degreeInput));
   alert(
-    isNaN(result)
-      ? `Вираз '${numInput} в ступені ${degreeInput}' не має сенсу.`
-      : `Число ${numInput} в ступені ${parseInt(
-          degreeInput
-        )} дорівнює ${result}`
+    `Число ${numInput.trim()} в ступені ${parseInt(degreeInput)} дорівнює ${result}`
   );
 }
